84e1201631983c7606c325d25b8dcaab9f9779b4
Andro Ostoic <androstoic@gmail.com>
2016-12-08 20:36:29 -0300

    nueva version

--- app/app.js
+++ app/app.js
@@ -57,7 +57,7 @@ config(['$locationProvider', '$routeProvider',
     signOutButton.addEventListener('click', function() {
        firebase.auth().signOut();
 
-     window.location.href = '/angularjsproyectiziboss';
+     window.location.href = '/pruebadoorman';
      });
 
 }]);
