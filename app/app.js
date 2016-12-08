c6ea5421ef5be9cd56954f07be6b152d46ec184b
Andro Ostoic <androstoic@gmail.com>
2016-12-07 23:40:43 -0300

    nueva version

--- app/app.js
+++ app/app.js
@@ -46,18 +46,18 @@ config(['$locationProvider', '$routeProvider',
 
 
 
-  var signOutButton = document.getElementById('sign-out-button');
-    var signOutButton2 = document.getElementById('sign-out-button2');
+ // var signOutButton = document.getElementById('sign-out-button');
+ //   var signOutButton2 = document.getElementById('sign-out-button2');
+    //
+    // signOutButton2.addEventListener('click', function() {
+    //  firebase.auth().signOut();
 
-    signOutButton2.addEventListener('click', function() {
-      firebase.auth().signOut();
-
-      window.location.href = '/angularjsproyectiziboss';
-    });
-  signOutButton.addEventListener('click', function() {
-    firebase.auth().signOut();
-
-    window.location.href = '/pruebadoorman';
-  });
+    //   window.location.href = '/angularjsproyectiziboss';
+    //  });
+    //signOutButton.addEventListener('click', function() {
+    //   firebase.auth().signOut();
+    //
+    //  window.location.href = '/pruebadoorman';
+    // });
 
 }]);
