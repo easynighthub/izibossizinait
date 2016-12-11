2723ba72f866647cb59b82c4f5e85f26bbd55d5f
Samuel Reinoso <hackeadorazo@gmail.com>
2016-12-11 14:01:47 -0400

    don't allow create duplicate doorman

--- app/createDoorMan/createDoorMan.js
+++ app/createDoorMan/createDoorMan.js
@@ -9,10 +9,9 @@ angular.module('myApp.createDoorMan', ['ngRoute'])
   });
 }])
 
-.controller('CreateDoorManCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+.controller('CreateDoorManCtrl', ['$scope', '$rootScope', '$firebaseObject', '$firebaseArray', '$filter',
+  function($scope, $rootScope, $firebaseObject, $firebaseArray, $filter) {
 
-
-  
 	function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
@@ -22,10 +21,11 @@ angular.module('myApp.createDoorMan', ['ngRoute'])
 
     $('.modulo').text('Crear Doorman');
 
-  var addDoormanToAdmin = function(doormanId) {
+  var addDoormanToAdmin = function(doormanId, added) {
     firebase.database().ref('admins/'+ userId + '/doormans/' + doormanId).set(true).then(
       function(s){
-        console.log('los tres pasos bien ', s);
+        if (added)
+        alert('Este doorman ya existe, se agregara a su lista de doormans');
         document.location.href = '#!/doorman';
       }, function(e) {
         alert('Error, intente de nuevo');
@@ -35,25 +35,23 @@ angular.module('myApp.createDoorMan', ['ngRoute'])
   }
 
   var saveToFIrebase = function(doorman) {
-      firebase.database().ref('doormans/'+ doorman.uid).set(doorman).then(
-        function(s){
-          console.log('se guardo bien ', s);
-          addDoormanToAdmin(doorman.uid);
-        }, function(e) {
-          alert('Error, intente de nuevo');
-          console.log('se guardo mal ', e);
-        }
-      );
-    };
+    firebase.database().ref('doormans/'+ doorman.uid).set(doorman).then(
+      function(s){
+        addDoormanToAdmin(doorman.uid);
+      }, function(e) {
+        alert('Error, intente de nuevo');
+        console.log('se guardo mal ', e);
+      }
+    );
+  };
 
 	$scope.saveDoorman = function() {
 
-        $scope.clientEmail = document.getElementById('clientEmail').value;
-        $scope.name = document.getElementById('name').value;
-        $scope.password = document.getElementById('password').value;
+    $scope.clientEmail = document.getElementById('clientEmail').value;
+    $scope.name = document.getElementById('name').value;
+    $scope.password = document.getElementById('password').value;
 
-
-		if (!validateEmail($scope.clientEmail)) {
+    if (!validateEmail($scope.clientEmail)) {
       alert('Debe ingresar un email valido');
       return;
     }
@@ -69,22 +67,32 @@ angular.module('myApp.createDoorMan', ['ngRoute'])
       alert('Debe ingresar una contraseña de 6 caracteres minimos');
       return;
     }
-    firebase.auth().createUserWithEmailAndPassword($scope.clientEmail, $scope.password).then(
-      function(s) {
-        console.log('autentificado bien' + s);
-        var doorman = {
-          anonimous: false,
-          email: $scope.clientEmail,
-          emailVerified: false,
-          providerId: 'firebase',
-          uid: s.uid,
-          name: $scope.name
-        };
-        saveToFIrebase(doorman);
-      }, function(e) {
-        console.log(e);
+
+    var doormansRequest = $firebaseArray(firebase.database().ref('/doormans'));
+    doormansRequest.$loaded().then(function(){
+      var doormaExist = $filter('filter')(doormansRequest, {email: $scope.clientEmail});
+      console.log(doormaExist);
+      if (doormaExist.length > 0) {
+        addDoormanToAdmin(doormaExist[0].uid, true);
+      } else {
+        firebase.auth().createUserWithEmailAndPassword($scope.clientEmail, $scope.password).then(
+          function(s) {
+            var doorman = {
+              anonimous: false,
+              email: $scope.clientEmail,
+              emailVerified: false,
+              providerId: 'firebase',
+              uid: s.uid,
+              name: $scope.name
+            };
+            saveToFIrebase(doorman);
+          }, function(e) {
+            console.log(e);
+          }
+        );
       }
-    );
+    });
+
 	}
 
 
